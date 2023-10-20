7066d673b86ba846dfa80912e8747d1906097498
siudn <cshadx@gmail.com>
2023-10-20 00:39:02 -0400

    Completed routing, created file architecture

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import {
   useLocation
 } from 'react-route-dom';
 import Home from "./scenes/home/Home";
+import ItemDetails from './scenes/itemDetails/ItemDetails';
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -23,6 +26,9 @@ function App() {
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="item/:itemId" element={<ItemDetails />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="checkout/success" element={<Confirmation />} />
       </Routes>
     </BrowserRouter>
   </div>
