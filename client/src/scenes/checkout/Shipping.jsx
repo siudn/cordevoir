a8b864f3a45f7d3123e826c03cb8c5364c57fe48
siudn <cshadx@gmail.com>
2023-10-24 00:58:16 -0400

    Checkout page with fields completed

--- client/src/scenes/checkout/Shipping.jsx
+++ client/src/scenes/checkout/Shipping.jsx
@@ -18,7 +18,7 @@ const Shipping = ({
         </Typography>
         <AddressForm
           type="billingAddress"
-          value={values.billingAddress}
+          values={values.billingAddress}
           errors={errors}
           touched={touched}
           handleBlur={handleBlur}
@@ -32,7 +32,7 @@ const Shipping = ({
           control={
             <Checkbox
               defaultChecked
-              value={values.shippingAddress.isSameAddress}
+              values={values.shippingAddress.isSameAddress}
               onChange={() =>
                 setFieldValue(
                   "shippingAddress.isSameAddress",
@@ -52,7 +52,7 @@ const Shipping = ({
           </Typography>
           <AddressForm
             type="shippingAddress"
-            value={values.shippingAddress}
+            values={values.shippingAddress}
             errors={errors}
             touched={touched}
             handleBlur={handleBlur}
