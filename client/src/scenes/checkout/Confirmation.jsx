f021df999d5ada515b84d9a37347cb3c9aa90f4f
siudn <cshadx@gmail.com>
2023-10-24 02:30:02 -0400

    Stripe fully integrated. Project complete.
    Also fixed bugs that were showing up as typescript errors, for some reason,
    using // @ts-ignore. Filled in confirmation page for successful order.

--- client/src/scenes/checkout/Confirmation.jsx
+++ client/src/scenes/checkout/Confirmation.jsx
@@ -1,5 +1,15 @@
+import { Box, Alert, AlertTitle } from "@mui/material";
+
 const Confirmation = () => {
-    return <div>Confirmation</div>;
-}
+  return (
+    <Box m="90px auto" width="80%" height="50vh">
+      <Alert severity="success">
+        <AlertTitle>Success</AlertTitle>
+        You have successfully made an Order —{" "}
+        <strong>Congrats on Making your Purchase!</strong>
+      </Alert>
+    </Box>
+  );
+};
 
 export default Confirmation;
