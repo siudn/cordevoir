035a5b99a2b6b1f0d7b43a5d38e54d906123ea6e
siudn <cshadx@gmail.com>
2023-10-24 09:52:03 -0400

    Revert

--- server/config/server.js
+++ server/config/server.js
@@ -1,11 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-<<<<<<< HEAD
   port: env.int("PORT", 1337),
-=======
-  port: env.int("PORT"),
-  url: "https://e-comm-backend-hlvb.onrender.com/",
->>>>>>> parent of 9c9e81b (Added proxy: true and links to admin.js)
   app: {
     keys: env.array("APP_KEYS"),
   },
