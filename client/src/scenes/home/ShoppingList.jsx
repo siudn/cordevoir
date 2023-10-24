8fd965adacd38dda12dc56cd5106dc553d10bd37
siudn <cshadx@gmail.com>
2023-10-24 10:07:19 -0400

    Full revert to local file state
    Before any attempts at deploying

--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -17,7 +17,7 @@ const ShoppingList = () => {
 
   async function getItems() {
     const items = await fetch(
-      "https://e-comm-backend-hlvb.onrender.com/api/items?populate=image",
+      "http://localhost:1337/api/items?populate=image",
       { method: "GET" }
     );
     const itemsJson = await items.json();
